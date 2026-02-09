1a3af837dff5d81a8ca265616e04328dfbd6b2a6
Kadian Ramkissoon <83883407+KadianR@users.noreply.github.com>
2026-02-09 07:56:25 -0400

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -1,66 +1,108 @@
+from typing_extensions import Annotated
 import typer
 from app.database import create_db_and_tables, get_session, drop_all
-from app.models import User, Todo, Category
+from app.models import User
 from fastapi import Depends
 from sqlmodel import select
 from sqlalchemy.exc import IntegrityError
 
 cli = typer.Typer()
 
-@cli.command()
+@cli.command(help = "Initializes the database and creates a default user 'bob")
 def initialize():
     with get_session() as db: # Get a connection to the database
         drop_all() # delete all tables
         create_db_and_tables() #recreate all tables
-        
-        bob = User(username='bob', email='bob@mail.com') # Create a new user (in memory)
-        bob.set_password("bobpass")
-
+        bob = User('bob', 'bob@mail.com', 'bobpass') # Create a new user (in memory)
         db.add(bob) # Tell the database about this new data
         db.commit() # Tell the database persist the data
         db.refresh(bob) # Update the user (we use this to get the ID from the db)
-
-        new_todo = Todo(text='Wash dishes', user_id=bob.id)
-
-        db.add(new_todo) # Tell the database about this new data
-        db.commit() # Tell the database persist the data
-        db.refresh(new_todo) # Update the user (we use this to get the ID from the db)
-
         print("Database Initialized")
 
-@cli.command()
-def add_task(username:str, task:str):
-    # Task 4.1 code here. Remove the line with "pass" below once completed
-    pass
+@cli.command(help = "Get a user by their username")
+def get_user(username:Annotated[str, typer.Argument(help = "The username of the user to retrieve")]):
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found!')
+            return
+        print(user)
 
-@cli.command()
-def toggle_todo(todo_id:int, username:str):
-    # Task 4.2 code here. Remove the line with "pass" below once completed
-    pass
+@cli.command(help = "Retrieves and prints all users from the database")
+def get_all_users():
+    with get_session() as db:
+        all_users = db.exec(select(User)).all()
+        if not all_users:
+            print("No users found")
+        else:
+            for user in all_users:
+                print(user)
 
-@cli.command()
-def list_todo_categories(todo_id:int, username:str):
-    # Task 5.3 code here. Remove the line with "pass" below once completed
-    pass
 
-@cli.command()
-def create_category(username:str, cat_text:str):        
-    # Task 5.4 code here. Remove the line with "pass" below once completed
-    pass
+@cli.command(help = "Updates a user's email by the given username")
+def change_email(username: Annotated[str, typer.Argument(help = "The username of the user whose email is to be updated")],                 
+                 new_email: Annotated[str, typer.Argument(help = "The new email address to update for the user")]):
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found! Unable to update email.')
+            return
+        user.email = new_email
+        db.add(user)
+        db.commit()
+        print(f"Updated {user.username}'s email to {user.email}")
 
-@cli.command()
-def list_user_categories(username:str):
-    # Task 5.5 code here. Remove the line with "pass" below once completed
-    pass
+@cli.command(help = "Creates a new user with the given username, email, and password and adds details to the database")
+def create_user(username: Annotated[str, typer.Argument(help = "The username of the new user to be created")], 
+                email: Annotated[str, typer.Argument(help = "The email address of the new user to be created")], 
+                password: Annotated[str, typer.Argument(help = "The password of the new user to be created")]):
+    with get_session() as db: # Get a connection to the database
+        newuser = User(username, email, password)
+        try:
+            db.add(newuser)
+            db.commit()
+        except IntegrityError as e:
+            db.rollback() #let the database undo any previous steps of a transaction
+            #print(e.orig) #optionally print the error raised by the database
+            print("Username or email already taken!") #give the user a useful message
+        else:
+            print(newuser) # print the newly created user
 
-@cli.command()
-def assign_category_to_todo(username:str, todo_id:int, category_text:str):
-    # Task 5.6 code here. Remove the line with "pass" below once completed
-    pass
-11. Conclusion
-Thus concludes your introduction to flask-sqlalchemy. The usage of this library is at the very core of this course.
+@cli.command(help = "Deletes a user by the given username from the database")
+def delete_user(username: Annotated[str, typer.Argument(help = "The username of the user to be deleted from the database")]):
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found! Unable to delete user.')
+            return
+        db.delete(user)
+        db.commit()
+        print(f'{username} deleted')
 
-You can view a completed version of this lab at the following link
+#Create a cli command that allows you to find a user using a partial match of their email OR username.
+@cli.command(help = "Finds and prints users whose username or email contains the given partial string")
+def find_user(query: Annotated[str, typer.Argument(help = "The partial string to search for in usernames and emails")]):
+    with get_session() as db:
+        users = db.exec(select(User).where((User.username.contains(query)) | (User.email.contains(query)))).all()
+        if not users:
+            print(f'No users found matching "{query}"')
+            return
+        for user in users:
+            print(user)
+
+#Create cli command that allows you to list the first N users of the database to be used by a paginated table. The command should accept 2 arguments limit and offset and return the appropriate result. limit should be defaulted to 10 and offset should be defaulted to 0
+@cli.command(help = "Lists users from the database by a paginated table using limit and offset values")
+def list_users(limit: Annotated[int, typer.Option(help = "The number of users to list (default 10)")] = 10, 
+               offset: Annotated[int, typer.Option(help = "The offset from the beginning of the list (default 0)")] = 0):
+    with get_session() as db:
+        users = db.exec(select(User).offset(offset).limit(limit)).all()
+        if not users:
+            print("No users found")
+            return
+        for user in users:
+            print(user)
+
+#Modify all the existing cli commands and add help statements for all arguments and documentation for all the functions
 
 if __name__ == "__main__":
     cli()
